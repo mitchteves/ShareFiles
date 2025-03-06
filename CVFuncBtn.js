097fce68dc6128374b13c124b4100c7a1dc67e02
Mitch Teves <61619615+mitchteves@users.noreply.github.com>
2025-03-06 14:19:10 +0900

    Update CVFuncBtn.js
    changed the call of the Terminal API
    FROM
    var runTrmFunc = await parent.TerminalApi.RunTerminalFunction(56, checkInfo, highlightedNode);
    TO
    var runTrmFunc = await parent.TerminalApi.RunTerminalFunction(56);

--- CVFuncBtn.js
+++ CVFuncBtn.js
@@ -1461,7 +1461,7 @@ async function PostTender() {
 
                 var checkInfo = await GetCheckObjectFromIG();
                 var highlightedNode = (isTest) ? 2 : await parent.TerminalApi.GetIndexOfHighlightedNode(checkInfo);
-                var runTrmFunc = await parent.TerminalApi.RunTerminalFunction(56, checkInfo, highlightedNode);
+                var runTrmFunc = await parent.TerminalApi.RunTerminalFunction(56);
 
                 await logToWorker(jsFunc + CL + "|RunTerminalFunction Status:" +
                     JSON.stringify(runTrmFunc, null, 2), LogLevel.INFO);

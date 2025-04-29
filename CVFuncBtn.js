4434dd1d832e02e3a316d38a35e0feebcabf0eef
Mitch Teves <61619615+mitchteves@users.noreply.github.com>
2025-04-29 10:35:12 +0900

    Update CVFuncBtn.js
    change to get all info of the check when post tender. need item list.

--- CVFuncBtn.js
+++ CVFuncBtn.js
@@ -1549,7 +1549,7 @@ async function PostTender() {
     var requestData = new RequestDataStructure();
 
     try {
-        var isProceed = await GetAllInfo(jsFunc, jsFunc, jsFunc, requestData, true, true);
+        var isProceed = await GetAllInfo(jsFunc, jsFunc, jsFunc, requestData, false, true);
 
         if (isProceed) {
             const sanizedRqData = deepStringify(requestData);

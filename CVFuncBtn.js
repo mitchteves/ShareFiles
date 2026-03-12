d4eb24609bb74c04f2349e15028ef83c4c4b5132
Mitch Teves <61619615+mitchteves@users.noreply.github.com>
2026-03-12 16:02:23 +0900

    update ccdc
    copy same param from ccdcnew.

--- CVFuncBtn.js
+++ CVFuncBtn.js
@@ -1261,6 +1261,10 @@ async function CCDiscount() {
                         requestData2.setAdditionalInfo({
                             MsgType: responseData.MsgType,
                             sMsg04: responseData.sMsg04,
+                            sMsg05: responseData.sMsg05,
+                            sMsg06: responseData.sMsg06,
+                            sMsg07: responseData.sMsg07,
+                            sMsg08: responseData.sMsg08,
                             ChkTaxAmt_BeforeDC: responseData.ChkTaxAmt_BeforeDC,
                             ChkGndTot_BeforeDC: responseData.ChkGndTot_BeforeDC,
                             ChkSvcChg_BeforeDC: responseData.ChkSvcChg_BeforeDC
